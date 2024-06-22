4ec583de0071d1185248b249d0dc48039abb2440
marcusviniciusbf86 <marcusviniciusbf86@gmail.com>
2024-06-22 18:06:29 -0300

    Ajuste19

--- terraform-deploy/azure-resources.tf
+++ terraform-deploy/azure-resources.tf
@@ -3,35 +3,35 @@
 # }
 
  resource "azurerm_resource_group" "rg_github" {
-   name     = var.resource_group_name
+   name     = "rg-tfstate-bra-dev"
    location = var.location
    tags = {
      environment = "Develop"
    }
  }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
+resource "azurerm_virtual_network" "vnet_github" {
+  name                = "vnet-github"
   address_space       = ["10.0.0.0/16"]
   location            = var.location
   resource_group_name = var.resource_group_name
 }
 
-resource "azurerm_subnet" "example" {
-  name                 = "internal"
+resource "azurerm_subnet" "subnet_github" {
+  name                 = "subnet-github-internal"
   resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.example.name
+  virtual_network_name = azurerm_virtual_network.subnet_github.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
+resource "azurerm_network_interface" "nic_github" {
+  name                = "nic-interface-github"
   location            = var.location
   resource_group_name = var.resource_group_name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
+    subnet_id                     = azurerm_subnet.subnet_github.id
     private_ip_address_allocation = "Dynamic"
   }
 }
@@ -41,14 +41,14 @@ resource "tls_private_key" "main" {
   rsa_bits  = 4096
 }
 
-resource "azurerm_linux_virtual_machine" "example" {
+resource "azurerm_linux_virtual_machine" "vm_github" {
   name                = "example-machine"
   resource_group_name = var.resource_group_name
   location            = var.location
   size                = "Standard_F2"
   admin_username      = "adminuser"
   network_interface_ids = [
-    azurerm_network_interface.example.id,
+    azurerm_network_interface.vnet_github.id,
   ]
 
   admin_ssh_key {
