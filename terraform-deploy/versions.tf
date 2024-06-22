c66d751b5073ff0417fefa0a310463c4f01b9934
marcusviniciusbf86 <marcusviniciusbf86@gmail.com>
2024-06-22 20:29:19 -0300

    Ajuste36

--- terraform-deploy/versions.tf
+++ terraform-deploy/versions.tf
@@ -14,10 +14,10 @@ terraform {
     storage_account_name = "stgbradevtfstate01"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
-    tenant_id            = "2f3876f5-e513-4806-8697-d5ef85d0c67e"
-    client_id            = "edfdf930-9929-4691-9f06-7e9951bc8140"
-    client_secret        = "2Ul8Q~oqg8rxtuYlwZ5wZzwHg_ZaFeK1Rfgd1cbg"
-    subscription_id      = "385a379e-e92e-4d3d-be57-ba91115c7e66"
+    # tenant_id            = "2f3876f5-e513-4806-8697-d5ef85d0c67e"
+    # client_id            = "edfdf930-9929-4691-9f06-7e9951bc8140"
+    # client_secret        = "2Ul8Q~oqg8rxtuYlwZ5wZzwHg_ZaFeK1Rfgd1cbg"
+    # subscription_id      = "385a379e-e92e-4d3d-be57-ba91115c7e66"
     # use_oidc             = true
   }
 }
