c1bd678900742f0eaf610097dbd5f90eff2aed22
marcusviniciusbf86 <marcusviniciusbf86@gmail.com>
2024-06-22 16:28:57 -0300

    Ajuste8

--- terraform-example-deploy/backend.tf
+++ terraform-example-deploy/backend.tf
@@ -1,12 +1,12 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-terraform-github-actions-state"
-    storage_account_name = "terraformgithubbra"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-   # access_key           = "EOOj6Gm8/aGscdQ/4WRBy55YaQQ1hlpujJX7FxMLF3x5NT0a/kxzHGxc2BVvxAmW6MFwRDdenkm9+AStX79e/g=="
-   } 
-  }
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name  = "rg-terraform-github-actions-state"
+#     storage_account_name = "terraformgithubbra"
+#     container_name       = "tfstate"
+#     key                  = "terraform.tfstate"
+#    # access_key           = "EOOj6Gm8/aGscdQ/4WRBy55YaQQ1hlpujJX7FxMLF3x5NT0a/kxzHGxc2BVvxAmW6MFwRDdenkm9+AStX79e/g=="
+#    } 
+#   }
  
 #   required_providers {
 #     azurerm = {
@@ -22,15 +22,15 @@ terraform {
 # }
 
 
-# terraform {
-#   required_providers {
-#     azurerm = {
-#       source = "hashicorp/azurerm"
-#       version = "=3.0.0"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
 
-# provider "azurerm" {
-#   features {}
-# }
+provider "azurerm" {
+  features {}
+}
