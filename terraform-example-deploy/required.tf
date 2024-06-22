5b787e45ab91ca800fc8ef09be051ff2a8083631
marcusviniciusbf86 <marcusviniciusbf86@gmail.com>
2024-06-22 16:03:59 -0300

    Send project1

--- terraform-example-deploy/required.tf
+++ terraform-example-deploy/required.tf
@@ -1,36 +1,36 @@
-# terraform {
-#   backend "azurerm" {
-#     resource_group_name  = "rg-tftec-state-remote"
-#     storage_account_name = "stomontstate"
-#     container_name       = "terraform"
-#     key                  = "terraform.tfstate"
-#     access_key           = "EOOj6Gm8/aGscdQ/4WRBy55YaQQ1hlpujJX7FxMLF3x5NT0a/kxzHGxc2BVvxAmW6MFwRDdenkm9+AStX79e/g=="
- 
-#   }
- 
-#   required_providers {
-#     azurerm = {
-#       source  = "hashicorp/azurerm"
-#       version = "3.96.0"
-#     }
- 
-#     random = {
-#       source  = "hashicorp/random"
-#       version = "3.6.0"
-#     }
-#   }
-# }
-
-
 terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-github-actions-state"
+    storage_account_name = "terraformgithubbra"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+   # access_key           = "EOOj6Gm8/aGscdQ/4WRBy55YaQQ1hlpujJX7FxMLF3x5NT0a/kxzHGxc2BVvxAmW6MFwRDdenkm9+AStX79e/g=="
+ 
+  }
+ 
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "=3.0.0"
+      source  = "hashicorp/azurerm"
+      version = "3.96.0"
+    }
+ 
+    random = {
+      source  = "hashicorp/random"
+      version = "3.6.0"
     }
   }
 }
 
-provider "azurerm" {
-  features {}
-}
+
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source = "hashicorp/azurerm"
+#       version = "=3.0.0"
+#     }
+#   }
+# }
+
+# provider "azurerm" {
+#   features {}
+# }
--- terraform-example-deploy/variables.tf
+++ terraform-example-deploy/variables.tf
@@ -1,3 +1,4 @@
 variable "resource_group_name" {
   type = string
+  default = "rg-terraform-github-actions-state"
 }
