a07512947ae06236ec54e19bbfc749378630366d
marcusviniciusbf86 <marcusviniciusbf86@gmail.com>
2024-06-22 16:27:49 -0300

    Ajuste7

--- terraform-example-deploy/backend.tf
+++ terraform-example-deploy/backend.tf
@@ -5,21 +5,21 @@ terraform {
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
    # access_key           = "EOOj6Gm8/aGscdQ/4WRBy55YaQQ1hlpujJX7FxMLF3x5NT0a/kxzHGxc2BVvxAmW6MFwRDdenkm9+AStX79e/g=="
- 
+   } 
   }
  
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.96.0"
-    }
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "3.96.0"
+#     }
  
-    random = {
-      source  = "hashicorp/random"
-      version = "3.6.0"
-    }
-  }
-}
+#     random = {
+#       source  = "hashicorp/random"
+#       version = "3.6.0"
+#     }
+#   }
+# }
 
 
 # terraform {
