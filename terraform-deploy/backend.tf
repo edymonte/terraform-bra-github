5514ee01e4a353889de91b2290c4d2cc07520b87
marcusviniciusbf86 <marcusviniciusbf86@gmail.com>
2024-06-22 19:38:30 -0300

    Ajuste31

--- terraform-deploy/backend.tf
+++ terraform-deploy/backend.tf
@@ -15,10 +15,12 @@ terraform {
     client_id            = "edfdf930-9929-4691-9f06-7e9951bc8140"
     client_secret        = "2Ul8Q~oqg8rxtuYlwZ5wZzwHg_ZaFeK1Rfgd1cbg"
     subscription_id      = "385a379e-e92e-4d3d-be57-ba91115c7e66"
+    use_oidc             = true
   }
 }
 provider "azurerm" {
   features {}
   alias               = "azurerm"
   subscription_id     = "385a379e-e92e-4d3d-be57-ba91115c7e66"
+  use_oidc             = true
 }
